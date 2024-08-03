0bc05844f1a5db5b3b4f642a035fe6a61c4b83a9
princekumar <princepk7878kumar@gmail.com>
2024-08-03 20:43:35 +0530

    done

--- src/pages/Toolbar/Toolbar.jsx
+++ src/pages/Toolbar/Toolbar.jsx
@@ -11,12 +11,15 @@ import heapSort from "../../algorithms/heapsort.js";
 import mergeSort from "../../algorithms/mergeSort.js";
 import selectionSort from "../../algorithms/selectionSort.js";
 import insertionSort from "../../algorithms/insertionSort.js";
+import AlgorithmsDetails from "../AlgorthitmInfo/AlgorthitmDetails.js";
 
 const Toolbar = () => {
   const dispatch = useDispatch();
   const array = useSelector((state) => state.array);
   const algorithm = useSelector((state) => state.toolbar.algorithm);
   const isRunning = useSelector((state) => state.running);
+
+  const [details, setDetails] = useState(null);
 
   const [arraySize, setArraySize] = useState(50);
   const [sortingSpeed, setSortingSpeed] = useState(50);
@@ -70,79 +73,121 @@ const Toolbar = () => {
   const color = isRunning ? "rgba(214, 29, 29, 0.8)" : "white";
   const cursor = isRunning ? "auto" : "pointer";
 
+  useEffect(() => {
+    const foundDetails = Object.values(AlgorithmsDetails).find(
+      (alg) => alg.name === algorithm
+    );
+    setDetails(foundDetails);
+  }, [algorithm]);
+
+  const sortingName=(name)=>{
+    if(name=="bubbleSort") return "Bubble Sort";
+    else if(name=="selectionSort") return "Selection Sort";
+    else if(name=="quickSort") return "Quick Sort";
+    else if(name=="heapSort") return "Heap Sort";
+    else if(name=="mergeSort") return "Merge Sort";
+    else if(name=="insertionSort") return "Insertion Sort";
+ }
+
   return (
-    <div id="toolbar" className="flex flex-col gap-5">
-      <div
-        id={!isRunning ? "generateArray" : "generateArrayX"}
-        style={{ color: color, cursor: cursor }}
-        onClick={!isRunning ? () => generateArray(array.length) : null}
-        type="button"
-        className="text-white bg-gradient-to-r font-bold font-serif  from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 text-xl rounded-lg px-5 py-2.5 text-center me-2 mb-2"
-      >
-        Generate New Array
-      </div>
-      <div id="arraySize" className="font-bold font-serif" style={{ color: color }}>
-        Change Array Size {`(${arraySize})`}
-      </div>
-      <input
-        id="changeArraySize"
-        type="range"
-        min="0"
-        max="100"
-        style={{ background: color, cursor: cursor }}
-        disabled={isRunning}
-        onChange={handleArraySizeChange}
-        value={arraySize}
-      />
-      <div id="sortingSpeed" className="font-bold font-serif" style={{ color: color }}>
-        Change Sorting Speed
-      </div>
-      <input
-        id="changeSortingSpeed"
-        type="range"
-        min="0"
-        max="100"
-        style={{ background: color, cursor: cursor }}
-        disabled={isRunning}
-        onChange={handleSortingSpeedChange}
-        value={sortingSpeed}
-      />
-      <select
-        id="algorithmSelect"
-        style={{ background: color, cursor: cursor }}
-        className="font-serif font-bold text-gray-900"
-        disabled={isRunning}
-        value={algorithm}
-        onChange={(e) => updateAlgorithm(e.target.value)}
-      >
-        <option value="" className="font-serif font-bold" disabled>
-          Select Sorting Algorithm
-        </option>
-        <option value="mergeSort" className="font-serif font-bold text-gray-900">Merge Sort</option>
-        <option value="quickSort" className="font-serif font-bold text-gray-900">Quick Sort</option>
-        <option value="heapSort" className="font-serif font-bold text-gray-900">Heap Sort</option>
-        <option value="bubbleSort" className="font-serif font-bold text-gray-900">Bubble Sort</option>
-        <option value="selectionSort" className="font-serif font-bold text-gray-900">Selection Sort</option>
-        <option value="insertionSort" className="font-serif font-bold text-gray-900">Insertion Sort</option>
-      </select>
-      <div className="flex">
-        {algorithm && (
-          <div
-            id="sort"
-            style={{ color: color, cursor: cursor }}
-            onClick={!isRunning ? () => sort(algorithm, array, speed) : null}
-          >
-            <button
-              type="button"
-              className="focus:outline-none font-bold font-serif text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
-              disabled={isRunning}
+    <>
+      <div id="toolbar" className="flex flex-col gap-5">
+        <div
+          id={!isRunning ? "generateArray" : "generateArrayX"}
+          style={{ color: color, cursor: cursor }}
+          onClick={!isRunning ? () => generateArray(array.length) : null}
+          type="button"
+          className="text-white bg-gradient-to-r font-bold font-serif from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 text-xl rounded-lg px-5 py-2.5 text-center me-2 mb-2"
+        >
+          Generate New Array
+        </div>
+        <div id="arraySize" className="font-bold font-serif" style={{ color: color }}>
+          Change Array Size {`(${arraySize})`}
+        </div>
+        <input
+          id="changeArraySize"
+          type="range"
+          min="0"
+          max="100"
+          style={{ background: color, cursor: cursor }}
+          disabled={isRunning}
+          onChange={handleArraySizeChange}
+          value={arraySize}
+        />
+        <div id="sortingSpeed" className="font-bold font-serif" style={{ color: color }}>
+          Change Sorting Speed
+        </div>
+        <input
+          id="changeSortingSpeed"
+          type="range"
+          min="0"
+          max="100"
+          style={{ background: color, cursor: cursor }}
+          disabled={isRunning}
+          onChange={handleSortingSpeedChange}
+          value={sortingSpeed}
+        />
+        <select
+          id="algorithmSelect"
+          style={{ background: color, cursor: cursor }}
+          className="font-serif font-bold text-gray-900"
+          disabled={isRunning}
+          value={algorithm}
+          onChange={(e) => updateAlgorithm(e.target.value)}
+        >
+          <option value="" className="font-serif font-bold" disabled>
+            Select Sorting Algorithm
+          </option>
+          <option value="mergeSort" className="font-serif font-bold text-gray-900">
+            Merge Sort
+          </option>
+          <option value="quickSort" className="font-serif font-bold text-gray-900">
+            Quick Sort
+          </option>
+          <option value="heapSort" className="font-serif font-bold text-gray-900">
+            Heap Sort
+          </option>
+          <option value="bubbleSort" className="font-serif font-bold text-gray-900">
+            Bubble Sort
+          </option>
+          <option value="selectionSort" className="font-serif font-bold text-gray-900">
+            Selection Sort
+          </option>
+          <option value="insertionSort" className="font-serif font-bold text-gray-900">
+            Insertion Sort
+          </option>
+        </select>
+        <div className="flex">
+          {algorithm && (
+            <div
+              id="sort"
+              style={{ color: color, cursor: cursor }}
+              onClick={!isRunning ? () => sort(algorithm, array, speed) : null}
             >
-              Sort!
-            </button>
-          </div>
-        )}
+              <button
+                type="button"
+                className="focus:outline-none font-bold font-serif text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+                disabled={isRunning}
+              >
+                Sort!
+              </button>
+            </div>
+          )}
+        </div>
       </div>
-    </div>
+      {details && (
+        <div className="flex justify-center flex-col text-white gap-5 mt-10 ">
+                <h2 className="text-2xl   font-serif  font-bold ">{sortingName(details.name)}</h2>
+
+          <p className="text-lg font-semibold font-serif">
+            Time Complexity: {details.timeComplexity}
+          </p>
+          <p className="text-lg font-semibold font-serif">
+            Space Complexity: {details.spaceComplexity}
+          </p>
+        </div>
+      )}
+    </>
   );
 };
 
