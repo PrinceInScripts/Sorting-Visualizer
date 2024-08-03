8defa5f430440b4639bbb4cbfb0495a7f45a33e8
princekumar <princepk7878kumar@gmail.com>
2024-08-03 23:43:41 +0530

    done project

--- src/pages/Toolbar/Toolbar.jsx
+++ src/pages/Toolbar/Toolbar.jsx
@@ -378,7 +378,7 @@ const Toolbar = () => {
                 className="focus:outline-none font-bold font-serif text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
                 disabled={isRunning}
               >
-                Sort!
+                Sort !
               </button>
             </div>
           )}
@@ -390,9 +390,9 @@ const Toolbar = () => {
           <p className="text-lg font-semibold font-serif">
             Time Complexity
             <ol>
-              <li>Best Case: {details.timeComplexity.bestCase}</li>
-              <li>Average Case: {details.timeComplexity.averageCase}</li>
-              <li>Worst Case: {details.timeComplexity.worstCase}</li>
+              <li>{details.timeComplexity.bestCase}</li>
+              <li>{details.timeComplexity.averageCase}</li>
+              <li>{details.timeComplexity.worstCase}</li>
             </ol>
           </p>
           <p className="text-lg font-semibold font-serif">
